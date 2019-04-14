4d3cd6d5f6f10fce6505e05b003ad60ed52323cb
Qbisst <ismaelaperez@outlook.com>
2019-04-14 18:03:35 -0400

    get concert working

--- liri.js
+++ liri.js
@@ -7,7 +7,6 @@ var Spotify = require("node-spotify-api");
 var moment = require("moment");
 var axios = require("axios");
 var fs = require("fs");
-var request = require("request");
 
 //Optional dependency set with const so the variable is not re-assigned//
 const chalk = require("chalk");
@@ -16,21 +15,42 @@ const chalk = require("chalk");
 var userOption = process.argv[2];
 var userTerm = process.argv.slice(3).join(" ");
 
-var userPicks = function(userData, functionData) {
-    switch (userData) {
+function start() {
+    switch (userOption) {
         case "concert-this":
-            getConcert(functionData);
+            getConcert();
             break;
         case "spotify-this-song":
-            getSpotify(functionData);
+            getSpotify();
             break;
         case "movie-this":
-            getMovie(functionData);
+            getMovie();
             break;
         case "do-what-it-says":
-            getWhat(functionData);
+            getWhat();
             break;
         default:
             console.log("LIRI does not know that")
     }
 };
+
+//Function to find concerts//
+function getConcert() {
+    axios.get("https://rest.bandsintown.com/artists/" + userTerm + "/events?app_id=codingbootcamp").then(function (response) {
+        var jsonData = response.data[0];
+        //moment.js variable to get date and time format//
+        dateGet = moment(jsonData.datetime).format("MM/DD/YYY");
+        // variable that will have the concert information//
+        var concertGet = `
+ Name of the venue: ${chalk.green(jsonData.venue.name)}
+ Venue location:    ${chalk.blue(jsonData.venue.city)}    
+ Date of the Event: ${chalk.magenta(dateGet)}
+ `;
+    
+    console.log(concertGet);
+
+    });
+
+};
+
+
