22de4e5f94fa0add4e7d9b80e4be1ab57658e109
Qbisst <ismaelaperez@outlook.com>
2019-04-14 15:20:16 -0400

    added user input functionality

--- liri.js
+++ liri.js
@@ -12,3 +12,25 @@ var request = require("request");
 //Optional dependency set with const so the variable is not re-assigned//
 const chalk = require("chalk");
 
+// User options//
+var userOption = process.argv[2];
+var userTerm = process.argv.slice(3).join(" ");
+
+var userPicks = function(userData, functionData) {
+    switch (userData) {
+        case "concert-this":
+            getConcert(functionData);
+            break;
+        case "spotify-this-song":
+            getSpotify(functionData);
+            break;
+        case "movie-this":
+            getMovie(functionData);
+            break;
+        case "do-what-it-says":
+            getWhat(functionData);
+            break;
+        default:
+            console.log("LIRI does not know that")
+    }
+};
